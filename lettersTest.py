ef81019cfef31d64718b60c2f2c9aee876ea062c
RiccardoMurgia <64796293+RiccardoMurgia@users.noreply.github.com>
2020-05-27 21:06:42 +0200

    Add files via upload

--- lettersTest.py
+++ lettersTest.py
@@ -1,23 +1,24 @@
-from  processesDataSet import *
+from processesDataSet import *
 from sklearn import tree
 from sklearn.metrics import accuracy_score
 from matplotlib import pyplot as plt
 from numpy import load
 
+
 def lettersTest():
     dimensions = [1024, 2048, 4096, 8192, 16384, 32768, 65536, 124800]
-    test_accuracy = []
-    train_accuracy = []
+    testAccuracy = []
+    trainAccuracy = []
 
     print("Letters classifier")
-    saveDataSet('letters')
+    '''saveDataSet('letters')'''
 
     # Extract the reshape Dataset
     images_train_init, labels_train_init = balanceDataSet(load("images_letters_train.npy"),
                                                           load("labels_letters_train.npy"))
 
     for dimension in dimensions:
-        print("The Decision Tree are training with " + str(dimension) + " elements")
+        print("The Decision Tree is training with " + str(dimension) + " elements")
 
         images_train, labels_train = images_train_init[0:dimension:], labels_train_init[0:dimension]
 
@@ -26,18 +27,20 @@ def lettersTest():
         clf = clf.fit(images_train, labels_train)
 
         # testing dataset
-        test_accuracy.append(
+        testAccuracy.append(
             accuracy_score(load("labels_letters_test.npy"), clf.predict(load("images_letters_test.npy"))))
-        train_accuracy.append(
+        trainAccuracy.append(
             accuracy_score(load("labels_letters_train.npy"), clf.predict(load("images_letters_train.npy"))))
 
-    plt.plot(dimensions, test_accuracy, marker="o", color='blue', label='TestAccuracy')
-    plt.plot(dimensions, train_accuracy, marker="o", color='green', label='TrainAccuracy')
+    plt.plot(dimensions, testAccuracy, marker="o", color='blue', label='TestAccuracy')
+    plt.plot(dimensions, trainAccuracy, marker="o", color='green', label='TrainAccuracy')
     plt.legend(loc='upper left')
     plt.title("Decision tree Performance in letters classifications")
     plt.xlabel("Training-Set Dimension")
-    plt.ylabel("Test Accuracy")
+    plt.ylabel("Accuracy")
 
+    plt.savefig('LettersAccuracy.png')
     plt.show()
+
 
 lettersTest()
